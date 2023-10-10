ff107712c579131ff10a1e7c8d8a7624825f5f9b
Jorge Alberto Niño Cabal <jorgenino_business@hotmail.com>
2023-10-09 22:05:51 -0600

    Remove coverage html

--- run_tests.sh
+++ run_tests.sh
@@ -6,8 +6,5 @@ coverage erase
 # Run the Django tests using coverage
 coverage run --source='.' manage.py test
 
-# Create an HTML report of the coverage
-coverage html
-
 # Display a brief summary in the terminal
 coverage report
